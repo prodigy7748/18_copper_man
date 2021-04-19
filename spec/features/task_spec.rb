7835e1bd215f97e3d002860267d072e3c2d3d6a2
Phil Lee <prodigy7748g@gmail.com>
2021-04-19 23:51:55 +0800

    add visit task index page test

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:title) { Faker::Lorem.sentence }
+  let(:content) { Faker::Lorem.paragraph }
+  let(:task) { create(:task, title: title, content: content) }
+
+  describe 'user visit task index page' do
+    it 'should show all tasks' do
+      3.times { create(:task) }
+      visit tasks_path
+
+      titles = all('#task_index_table tr > td:first-child').map(&:text)
+      result = Task.pluck(:title)
+      expect(titles).to eq result
+    end
+  end
 end
