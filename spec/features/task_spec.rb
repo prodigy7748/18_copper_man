b82ba45a640035b98f286ded131c610e85bb4a90
Phil Lee <prodigy7748g@gmail.com>
2021-04-20 10:49:19 +0800

    add task update test

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -42,10 +42,44 @@ RSpec.feature "Tasks", type: :feature do
     end
   end
 
+  describe 'update tasks' do
+    let(:new_title) { Faker::Lorem.sentence }
+    let(:new_content) { Faker::Lorem.paragraph }
+
+    scenario 'with new_title and new_content' do
+      update_task(title: new_title, content: new_content)
+      expect(page).to have_content(new_title)
+      expect(page).to have_content(new_content)
+    end
+
+    scenario 'without title and content' do
+      update_task()
+      expect(page).to have_content('Title can\'t be blank')
+      expect(page).to have_content('Content can\'t be blank')
+    end
+
+    scenario 'without title' do
+      update_task(content: new_content)
+      expect(page).to have_content('Title can\'t be blank')
+    end
+
+    scenario 'without content' do
+      update_task(title: new_title)
+      expect(page).to have_content('Content can\'t be blank')
+    end
+  end
+
   def create_task(title: nil, content: nil)
     visit new_task_path
     fill_in '任務名稱', with: title
     fill_in '內容', with: content
     click_button '新增任務'
+  end
+
+  def update_task(title: nil, content: nil)
+    visit edit_task_path(task)
+    fill_in '任務名稱', with: title
+    fill_in '內容', with: content
+    click_button '更新任務'
   end
 end
