02cfce880b0cc8fa2b727538fbcae624244839d6
Phil Lee <prodigy7748g@gmail.com>
2021-04-20 10:41:53 +0800

    refine task create test

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -18,38 +18,34 @@ RSpec.feature "Tasks", type: :feature do
   end
 
   describe 'creates a new task' do
-    before do 
-      visit new_task_path
-    end
-
     scenario 'with title and content' do
-      fill_in 'Title', with: title
-      fill_in 'Content', with: content
-      click_button 'Create Task'
-      expect { create(:task) }.to change { Task.count }.by(1)
+      expect{ create_task(title: title, content: content) }.to change { Task.count }.by(1)
       expect(page).to have_content('新增任務成功！')
       expect(page).to have_content(title)
       expect(page).to have_content(content)
     end
 
     scenario 'without title and content' do
-      fill_in 'Title', with: ''
-      fill_in 'Content', with: ''
-      click_button 'Create Task'
+      create_task()
       expect(page).to have_content('Title can\'t be blank')
       expect(page).to have_content('Content can\'t be blank')
     end
 
     scenario 'without title' do
-      fill_in 'Title', with: ''
-      click_button 'Create Task'
+      create_task(content: content)
       expect(page).to have_content('Title can\'t be blank')
     end
 
     scenario 'without content' do
-      fill_in 'Content', with: ''
-      click_button 'Create Task'
+      create_task(title: title)
       expect(page).to have_content('Content can\'t be blank')
     end
+  end
+
+  def create_task(title: nil, content: nil)
+    visit new_task_path
+    fill_in '任務名稱', with: title
+    fill_in '內容', with: content
+    click_button '新增任務'
   end
 end
