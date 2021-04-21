05f770c15ec40f8f02205d001b8f010daae9fa57
Phil Lee <prodigy7748g@gmail.com>
2021-04-21 11:25:15 +0800

    add priority and status column

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,12 @@
 class Task < ApplicationRecord
+  enum priority: { low: 0, medium: 1, high: 2 }
+  enum status: { pending: 0, processing: 1, completed: 2 }
+
   validates :title, presence: true
   validates :content, presence: true
 
   scope :sorted_by, ->(sort_option) {
-    direction = /desc$/.match?(sort_option) ? "desc" : "asc"
+    direction = /desc$/.match?(sort_option) ? "asc" : "desc"
     case sort_option.to_s
     when /^created_at_/
       order("created_at #{direction}")
